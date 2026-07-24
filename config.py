ccc2bb4428fcd995ea560fe7e1c85d849b805040
Rizky Azis Pratama <rizkyazis567@gmail.com>
2026-07-24 21:27:32 +0700

    Update database config

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ DB_CONFIG = {
     'user': 'students_task_manager_caveuseful',         
     'password': '353cb13776b5e11871a13095c73b76a0e5fad8b2',        
     'database': 'students_task_manager_caveuseful',
-    'port: 3307,
+    'port': 3307,
     'charset': 'utf8mb4'
 }
 
