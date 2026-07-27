78fb9f7bd1d3f8482720d86747ccac03e9b599a6
wihbi <tegarjuga441@gmail.com>
2026-07-28 02:47:22 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,5 @@
 # config.py - pengaturan koneksi database dan secret key
-# edit bagian ini kalau ganti server atau environment
 
-# setting koneksi ke MariaDB via XAMPP
-# kalau pakai hosting lain, sesuaikan host, user, dan passwordnya
-DB_CONFIG = {
     'host': 'uowoto.h.filess.io',      
     'user': 'students_task_manager_caveuseful',         
     'password': '353cb13776b5e11871a13095c73b76a0e5fad8b2',        
@@ -13,5 +9,4 @@ DB_CONFIG = {
 }
 
 # secret key buat enkripsi session Flask
-# ganti ini dengan string random yang panjang kalau mau deploy!
 SECRET_KEY = 'student_task_manager_secret_2024'
