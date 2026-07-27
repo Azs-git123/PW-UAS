ee9d23bc85edea873472329984add616c5cf87bd
wihbi <tegarjuga441@gmail.com>
2026-07-28 03:00:43 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 # config.py - pengaturan koneksi database dan secret key
 
+DB_CONFIG = {
     'host': 'uowoto.h.filess.io',      
     'user': 'students_task_manager_caveuseful',         
     'password': '353cb13776b5e11871a13095c73b76a0e5fad8b2',        
